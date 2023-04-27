8cdac59194d0080d17087b76c3aaad5c1be48b54
Nurbeniz <102666235+nuryagli@users.noreply.github.com>
2023-04-27 21:12:34 +0300

    RemovedHomeScreen

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { LoginScreen } from './screens/LoginScreen';
 import { RegisterScreen } from './screens/RegisterScreen';
 import { PasswordRecoveryScreen } from './screens/PasswordRecoveryScreen';
 import { NewPasswordScreen } from './screens/NewPasswordScreen';
-import { HomeScreen } from './screens/HomeScreen';
 
 
 const Stack = createStackNavigator();
@@ -16,17 +15,13 @@ const App = () => {
     <NavigationContainer>
       <StatusBar style='light' />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} /> */}
+        <Stack.Screen name="Register" component={RegisterScreen} /> 
         <Stack.Screen name="PasswordRecovery" component={PasswordRecoveryScreen} />
         <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-      
-
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
 export default App;
